9d278a6fbbe9763af2a008f40b1424e65d706e91
mukuljeswani <mukuljeswani@gmail.com>
2022-05-06 13:56:05 -0400

    hello world complete

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import "./style.css"
+import Button from './Button';
 
 const App = () => {
     return (
         <div>
             <h1 style={{ textAlign: "center" }}>Hello World</h1>
-            <button className = "button">Heyy</button>
+            {/* <button className = "button">Heyy</button> */}
+            <Button title = "App Store" />
+            <Button title = "Play Store"/>
         </div>
     );
 }
